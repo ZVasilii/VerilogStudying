f45203d852c83065f855821291667b2263f3341f
ZVasilii <zcvasilii@gmail.com>
2023-02-14 00:07:29 +0300

    Add manual and play w/ buttons

--- blink/top.v
+++ blink/top.v
@@ -1,10 +1,18 @@
-module top( input CLK,
+module top( input CLK, KEY1,
 				output DS_EN1, DS_EN2, DS_EN3, DS_EN4,
 				output DS_C);
 
 wire clkDiv;
+reg clkButton;
 assign {DS_EN1, DS_EN2, DS_EN3, DS_EN4} = 4'hf;
 clk_div clk_div01(.clk(CLK), .clkDiv(clkDiv));
-assign DS_C = clkDiv;
+
+always @(clkDiv or KEY1)
+begin
+	if (KEY1)
+		clkButton = clkDiv;
+end
+
+assign DS_C = clkButton;
 
 endmodule
